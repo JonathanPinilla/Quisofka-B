3ac03807b0e4af78ab0a2d68919b638a18a6ce61
Difsan <difsanchezru@gmail.com>
2023-04-24 11:58:57 -0500

    first commit

---
+++ src/test/java/com/quisofka/quizzes/QuizzesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quisofka.quizzes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
